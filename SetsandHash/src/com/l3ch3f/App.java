6915a4e6d046121960c7e4a4a2347092d07e9408
l3ch3f <mfsoeropaiman@gmail.com>
2021-01-30 17:45:33 +0100

    init sets and hash

---
+++ SetsandHash/src/com/l3ch3f/App.java
@@ -0,0 +1,10 @@
+package com.l3ch3f;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ SetsandHash/src/com/l3ch3f/HeavenlyBody.java
@@ -0,0 +1,7 @@
+package com.l3ch3f;
+
+public final class HeavenlyBody {
+	private final String name;
+	private final double orbintalPeriod;
+	private final Set<HeavenlyBody>;
+}
