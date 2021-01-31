a3c2b8c215a8a0c70b7d83aee5e6651d31cbf42f
l3ch3f <mfsoeropaiman@gmail.com>
2021-01-31 14:51:39 +0100

    wrote the sortedCollections

---
+++ sortedCollections/src/sortedCollections/StockItem.java
@@ -0,0 +1,94 @@
+package sortedCollections;
+
+public class StockItem implements Comparable<StockItem> {
+	private final String name;
+	private double price;
+//	private int quantityStock = 0; // can be init later
+
+	public StockItem(String name, double price) {
+		this.name = name;
+		this.price = price;
+		this.quantityStock = 0; // or here(but you wouldnt do both normally)
+	}
+	
+	
+
+	public StockItem(String name, double price, int quantityStock) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quantityStock = quantityStock;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		if(price > 0.0) {
+			this.price = price;
+		}
+			
+	}
+
+	public int getQuantityStock() {
+		return quantityStock;
+	}
+
+	
+	public void adjustStock(int quantity) {
+		int newQuantity = this.quantityStock + quantity;
+		if(newQuantity >= 0) {
+			this.quantityStock = newQuantity;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("Entering StockItem.equals");
+		if(obj== this) {
+			return true;
+		}
+		if((obj==null) || (obj.getClass() != this.getClass())) {
+			return false;
+		}
+		
+		String objName = ((StockItem) obj).getName();
+		return this.name.equals(objName);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.name.hashCode() + 31;
+	}
+
+	@Override
+	public int compareTo(StockItem o) {
+		System.out.println("Entering StockItem.compareTo");
+		if(this == o) {
+			return 0;
+		}
+		if(o != null) {
+			return this.name.compareTo(o.getName());
+		}
+		
+		throw new NullPointerException();
+	}
+
+	@Override
+	public String toString() {
+		return this.name + " : price " + this.price;
+	}
+	
+	
+	
+}
+
+	
