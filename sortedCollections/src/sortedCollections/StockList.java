06a2deefafbe9c6495d33ce67d4257106adc489e
l3ch3f <mfsoeropaiman@gmail.com>
2021-01-31 15:51:00 +0100

    changed to linkedHashmap for sorting

--- sortedCollections/src/sortedCollections/StockList.java
+++ sortedCollections/src/sortedCollections/StockList.java
@@ -1,16 +1,14 @@
 package sortedCollections;
 
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StockList {
 	private final Map<String, StockItem> list;
 	
 	public StockList() {
-		this.list = new HashMap<>();
+		this.list = new LinkedHashMap<>();
 	}
 	
 	public int addStock(StockItem item) {
@@ -54,10 +52,10 @@ public class StockList {
 			double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
 			
 			s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
-			s = s + itemValue + "\n";
+			s = s + String.format("%.2f", itemValue) + "\n";
 			totalCost += itemValue;
 		}
-		return s + "Total stock value" + totalCost;
+		return s + "Total stock value " + totalCost;
 	}
 	
 	
